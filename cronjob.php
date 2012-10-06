1e75c01afd205c65073afd887f6a85999071c5fd
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-10-06 20:50:10 +0200

    cronjob access file

---
+++ cronjob.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * gitMD2CMS
+ *
+ * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/gitMD2CMS
+ * @copyright 2012 phpManufaktur by Ralf Hertsch
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace gitMD2CMS;
+
+use gitMD2CMS\GitHub\Access;
+
+
+require_once '../../config.php';
+require_once 'GitHub/Access.php';
+
+$access = new Access();
+$access->action();
